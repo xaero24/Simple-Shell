9dad0e92830bc28ae4361fbb4674790647096fc1
Mike A <michaelafu@gmail.com>
2020-04-10 21:17:16 +0300

    gitignore experiments

--- shell/reader.c
+++ shell/reader.c
@@ -5,17 +5,22 @@
 
 int parseLine(char *cmdLine, dataLine *parsed)
 {
-    int words = 1, i = 0;
-    char s;
+    int words = 0, i = 0, j = 0;
+    char s, word[256];
     printf("%s\n", cmdLine);
     do
     {
+        j = 0;
         s = cmdLine[i++];
-        if(s == ' ')
+        while(s != ' ' && s != '\n')
         {
             /*Get each word into a temp variable and store it*/
-            words++;
+            word[j++] = s;
+            s = cmdLine[i++];
         }
+        word[j] = '\0';
+        printf("Word is \"%s\"\n", word);
+        words++;
     } while (s != '\n');
     
     printf("Entered %d parameters\n", words);
